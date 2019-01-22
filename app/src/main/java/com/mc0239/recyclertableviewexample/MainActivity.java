eddde829d38f2f7a5627c08817d99b879c5e91ce
Martin <martin.cebular@gmail.com>
2019-01-22 22:29:07 +0100

    Added samples to app

--- app/src/main/java/com/mc0239/recyclertableviewexample/MainActivity.java
+++ app/src/main/java/com/mc0239/recyclertableviewexample/MainActivity.java
@@ -1,66 +1,92 @@
 package com.mc0239.recyclertableviewexample;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.util.SparseArray;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
-import com.mc0239.recyclertableview.RecyclerTableView;
-import com.mc0239.recyclertableview.RecyclerTableViewAdapter;
+import com.mc0239.recyclertableviewexample.samples.FragmentSampleCheckbox;
+import com.mc0239.recyclertableviewexample.samples.FragmentSampleEdittext;
+import com.mc0239.recyclertableviewexample.samples.FragmentSampleRadiobutton;
+import com.mc0239.recyclertableviewexample.samples.FragmentSampleUsage;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
-    private RecyclerTableView recyclerTableView;
-    private RecyclerTableViewAdapter recyclerTableViewAdapter;
+    private Fragment
+            fragmentSampleUsage,
+            fragmentSampleCheckbox,
+            fragmentSampleRadiobutton,
+            fragmentSampleEdittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Prepare some data for the table
-        ArrayList<SparseArray<Object>> data = new ArrayList<>();
-        for(int i = 0; i < 100; i++) {
-            SparseArray<Object> s = new SparseArray<>();
-            s.put(R.id.checkBoxSelected, false);
-            s.put(R.id.textViewID, i);
-            s.put(R.id.textViewUsername, "johnd");
-            s.put(R.id.textViewName, "John");
-            s.put(R.id.textViewSurname, "Doe");
-            data.add(s);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentSampleUsage = new FragmentSampleUsage();
+        fragmentSampleCheckbox = new FragmentSampleCheckbox();
+        fragmentSampleRadiobutton = new FragmentSampleRadiobutton();
+        fragmentSampleEdittext = new FragmentSampleEdittext();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawers();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        int mid = menuItem.getItemId();
+
+        if (mid == R.id.nav_sample) {
+            transaction.replace(R.id.fragment_frame, fragmentSampleUsage);
+        } else if (mid == R.id.nav_sample_checkbox) {
+            transaction.replace(R.id.fragment_frame, fragmentSampleCheckbox);
+        } else if (mid == R.id.nav_sample_radio) {
+            transaction.replace(R.id.fragment_frame, fragmentSampleRadiobutton);
+        } else if (mid == R.id.nav_sample_edittext) {
+            transaction.replace(R.id.fragment_frame, fragmentSampleEdittext);
+        } else {
+            Log.w(getClass().getSimpleName(), "Navigation menu selection not handled.");
         }
 
-        // Get RecyclerTableView view
-        recyclerTableView = findViewById(R.id.recyclerTableView1);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.disallowAddToBackStack();
+        transaction.commit();
 
-        // Set header of the table
-        recyclerTableView.setHeaderResource(R.layout.table_row_sample1, R.color.colorPrimary, R.color.colorWhite);
-
-        // Set TableView checkable by providing ID of the checkbox view
-        recyclerTableView.setCheckable(R.id.checkBoxSelected);
-
-        // Set LayoutManager
-        recyclerTableView.setLayoutManager(new LinearLayoutManager(this));
-
-        // Create a new RecyclerTableViewAdapter
-        recyclerTableViewAdapter = new RecyclerTableViewAdapter(
-                data,
-                R.layout.table_row_sample1,
-                new int[]{
-                        R.id.checkBoxSelected,
-                        R.id.textViewID,
-                        R.id.textViewUsername,
-                        R.id.textViewName,
-                        R.id.textViewSurname
-                },
-                R.id.checkBoxSelected);
-
-        // Set adapter to RecyclerTableView
-        recyclerTableView.setAdapter(recyclerTableViewAdapter);
-
+        return true;
     }
 }
---
+++ app/src/main/java/com/mc0239/recyclertableviewexample/samples/FragmentSampleRadiobutton.java
@@ -0,0 +1,74 @@
+package com.mc0239.recyclertableviewexample.samples;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mc0239.recyclertableview.RecyclerTableView;
+import com.mc0239.recyclertableview.RecyclerTableViewAdapter;
+import com.mc0239.recyclertableviewexample.R;
+
+import java.util.ArrayList;
+
+public class FragmentSampleRadiobutton extends Fragment {
+
+    private RecyclerTableView recyclerTableView;
+    private RecyclerTableViewAdapter recyclerTableViewAdapter;
+
+    public FragmentSampleRadiobutton() {
+        // Prepare some data for the table
+        ArrayList<SparseArray<Object>> data = new ArrayList<>();
+        for(int i = 0; i < 30; i++) {
+            SparseArray<Object> s = new SparseArray<>();
+            s.put(R.id.radioButtonSelected, false);
+            s.put(R.id.textViewID, i);
+            s.put(R.id.textViewUsername, "johnd");
+            s.put(R.id.textViewName, "John");
+            s.put(R.id.textViewSurname, "Doe " + i);
+            data.add(s);
+        }
+
+        // Create a new RecyclerTableViewAdapter
+        recyclerTableViewAdapter = new RecyclerTableViewAdapter(
+                data,
+                R.layout.table_row_sample_radio,
+                new int[]{
+                        R.id.radioButtonSelected,
+                        R.id.textViewID,
+                        R.id.textViewUsername,
+                        R.id.textViewName,
+                        R.id.textViewSurname
+                },
+                R.id.radioButtonSelected);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sample, container, false);
+
+        // Get RecyclerTableView view
+        recyclerTableView = view.findViewById(R.id.recyclerTableView1);
+
+        // Set header of the table
+        recyclerTableView.setHeaderResource(R.layout.table_row_sample_radio, R.color.colorPrimary, R.color.colorWhite);
+
+        // Set TableView checkable by providing ID of the checkbox view
+        recyclerTableView.setCheckable(R.id.radioButtonSelected);
+
+        // Set LayoutManager
+        recyclerTableView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Set adapter to RecyclerTableView
+        recyclerTableView.setAdapter(recyclerTableViewAdapter);
+
+        return view;
+    }
+
+}
